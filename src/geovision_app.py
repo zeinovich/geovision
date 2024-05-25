a4f10c2176d75fc9487af961fbfc99ce7ae1ccbf
egor.koptelov <egorkoptelov99@gmail.com>
2024-05-25 16:24:45 +0300

    update file uploading

--- src/geovision_app.py
+++ src/geovision_app.py
@@ -1,12 +1,51 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
+
 from PIL import Image
+
+import numpy as np
+import cv2
+from pdf2image import convert_from_bytes
+import os
+
+from utils import image_preprocessing
 
 
 def main():
-    image_name = st.text_input('Enter path to file:')
 
-    image = Image.open(image_name)
-    st.image(image)
+    st.set_option("deprecation.showPyplotGlobalUse", False)
+    st.set_page_config(layout='wide')
+    st.sidebar.markdown('# GeoVision')
+
+    with st.sidebar:
+        selected_step = option_menu(
+            menu_title='',
+            options=[
+                'Step 1: Upload image',
+                'Step 2: Image processing',
+                'Step 3: Result'
+            ],
+            menu_icon='display',
+            default_index=0
+        )
+
+    uploaded_file = st.sidebar.file_uploader("Upload image", type=['jpg', 'jpeg', 'png', 'pdf', 'tiff', 'cdr'])
+
+    if selected_step == 'Step 1: Upload image':
+        if uploaded_file:
+
+            st.title("Uploaded file")
+            # image = Image.open(uploaded_image)
+
+            image = image_preprocessing.load_and_display_file(uploaded_file)
+            st.image(image, use_column_width=True)
+
+    if selected_step == 'Step 2: Image processing':
+        pass
+
+    if selected_step == 'Step 3: Result':
+        pass
+
 
 if __name__ == "__main__":
     main()
--- src/utils/image_preprocessing.py
+++ src/utils/image_preprocessing.py
@@ -1,4 +1,20 @@
+from PIL import Image
 import cv2
+from pdf2image import convert_from_bytes
 
-def image_processing():
-    pass
+def load_and_display_file(uploaded_file):
+    file_type = uploaded_file.type.split('/')[-1].lower()
+
+    if file_type in ['jpg', 'jpeg', 'png', 'tiff', 'tif']:
+        image = Image.open(uploaded_file)
+    elif file_type == 'pdf':
+        # Используем pdf2image для конвертации PDF в изображение
+        images = convert_from_bytes(uploaded_file.read())
+        image = images[0]  # Берем первую страницу PDF
+    elif file_type == 'cdr':
+        return None
+    else:
+        st.error("Неподдерживаемый формат файла")
+        return None
+
+    return image
