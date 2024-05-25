9e020a51f42175a7b150e6b21d276bc118125288
Nikita.Zinovich <nikita.zinovich@gmail.com>
2024-05-25 15:20:22 +0300

    cv2 morphological closing

--- src/utils/image_preprocessing.py
+++ src/utils/image_preprocessing.py
@@ -1,4 +1,31 @@
 import cv2
+import numpy as np
 
-def image_processing():
-    pass
+
+def image_processing(
+    img: np.ndarray, kernel_size: int = 3, iterations: int = 1
+) -> np.ndarray:
+    """
+    `img`: np.ndarray
+        Image
+    `kernel_size`: int, default=3
+        Size of kernel for morphological operation (`cv2.MORPH_CLOSE`)
+    `iterations`: int, default=1
+        Number of iterations
+
+    Return
+        `np.ndarray` (img)
+    """
+    kernel = np.ones(
+        (kernel_size, kernel_size),
+        dtype=np.uint8,
+    )
+
+    img = cv2.morphologyEx(
+        img,
+        cv2.MORPH_CLOSE,
+        kernel=kernel,
+        iterations=iterations,
+    )
+
+    return img
