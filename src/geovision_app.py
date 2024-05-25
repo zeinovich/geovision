6de4d2087721eb3f97335d9eae4b823f8bba5785
Nikita.Zinovich <nikita.zinovich@gmail.com>
2024-05-25 19:06:08 +0300

    interim

--- src/geovision_app.py
+++ src/geovision_app.py
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-import streamlit as st
-
-from paddleocr import PaddleOCR
-from PIL import Image
-
-from utils.image_preprocessing import image_processing, get_ocr
-from utils.plot_scale import compute_depth_scale
-
-
-def main():
-    OCR_MODEL = PaddleOCR(
-        ocr_version="PP-OCRv4",
-        use_angle_cls=True,
-        lang="en",
-        use_gpu=False,
-        show_log=False,
-    )
-
-    image_name = st.text_input("Enter path to file:")
-
-    image = Image.open(image_name)
-
-    st.image(image)
-
-    preds = get_ocr(OCR_MODEL, image)
-
-
-if __name__ == "__main__":
-    main()
-=======
 import streamlit as st
 from paddleocr import PaddleOCR
 from PIL import Image
@@ -100,4 +69,3 @@ def main():
 
 if __name__ == "__main__":
     main()
->>>>>>> f087fa68870c09a850817311d0a18b342ae46a2c
